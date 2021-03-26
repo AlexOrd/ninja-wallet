af60ce0fcdd9446bae4b7936e571fe11b5aeb12d
Svetlana Kollau <svetlana.kollau@gmail.com>
2021-03-26 21:17:28 +0200

    add transaction model

---
+++ server/models/transaction.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    _id: {
+        type: Schema.ObjectId,
+        required: true
+    },
+    transactionName: {
+        type: String, 
+        required: true
+    },
+    transactionCategory: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'  
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true 
+    },
+    accountNumber: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Account',
+      required: true   
+    },
+    transactionCurrency: {
+        type: String,
+        required: true
+    },
+    sum: {
+      type: Number,
+      required: true
+    }
+});
+
+const Transaction = mongoose.model('Transaction', schema);
+
+export { Transaction };
