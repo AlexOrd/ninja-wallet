8beb6f335ba23bc6613e35645fe3eb67c1278748
Vitalii <ghvitaliidrapaliuk@gmail.com>
2021-03-28 17:23:50 +0300

    add auth part to user schama

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,10 +1,32 @@
 import mongoose from 'mongoose';
-
 const Schema = mongoose.Schema;
+
+const verificationCode = new mongoose.Schema({
+  _id: false,
+  value: {
+      type: String, 
+      required: true
+  },
+  emitted: {
+      type: Date,
+      required: true
+  }
+})
 
 const schema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  auth: {
+    email: String,
+    password: String,
+    refreshToken: String,
+    isVerificationEmail: {
+        type: Boolean,
+        default: false
+    },
+    changePasswordCode: verificationCode,
+    verificationCode: verificationCode,
+  }
 });
 
 const User = mongoose.model('User', schema);
