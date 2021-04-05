455df5baa3895c74dd7557ac7e5853c564886c51
Andrew <andriykokin@gmail.com>
2021-04-05 12:20:31 +0300

    Create model for profile

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,6 +5,11 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  userPhoto: {
+    type: Schema.Types.ObjectId,
+    ref: 'userPhoto',
+    required: true
+  },
 });
 
 const User = mongoose.model('User', schema);
