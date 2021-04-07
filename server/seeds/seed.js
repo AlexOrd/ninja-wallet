762a793b0f26fb5bd0eaa47e3782f302c34476ad
Svetlana Kollau <svetlana.kollau@gmail.com>
2021-04-07 11:01:45 +0300

    creating seed file for transactions

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -5,18 +5,22 @@ import seeder from 'mongoose-seed';
 const dbURI = process.env.MONGODB_URI;
 
 import {categoriesSeedData} from './categories.seed';  
+import {transactionsSeedData} from './transactions.seed';
 
 seeder.connect(dbURI, {useUnifiedTopology: true} ,function () {
   seeder.loadModels(([
     // path there your models
+    './server/models/transaction.model.js',
     './server/models/category.model.js'
   ]));
   seeder.clearModels([
     // path there your models name
-    'Category'
+    'Category',
+    'Transaction'
   ], () => console.log('cleared'));
   seeder.populateModels([
     // path there you objects with data
+    transactionsSeedData,
     categoriesSeedData
   ], (err, done) => {
     if (err) {
---
+++ server/seeds/transactions.seed.js
@@ -0,0 +1,21 @@
+export const transactionsSeedData = {
+  model: ‘Transaction’,
+  documents: [
+    {
+      _id: 92082309483290821,
+      transactionName: 'First',
+      transactionCategory: 'Products',
+      userId: '36336498',
+      accountId: '48793473',
+      merchantName: 'Silpo',
+    },
+    {
+      _id: 72082309483290821,
+      transactionName: 'Second',
+      transactionCategory: 'Medic',
+      userId: '36336498',
+      accountId: '48793473',
+      merchantName: 'Zdorovie',
+    }
+  ],
+};
