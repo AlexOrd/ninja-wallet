af76d7dd6eac588ce35f33a632be04bd15613c93
AntonDeXy <podolyananton@gmail.com>
2021-04-09 11:09:20 +0300

    implemented CRUD for categories with validations

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoutes from './auth.route';
 import userRoutes from './user.route';
+import categoriesRoutes from './categories.route';
 
 const router = express.Router();
 
@@ -9,5 +10,7 @@ router.use('/auth', authRoutes);
 
 // mount user routes at /users
 router.use('/users', userRoutes);
+
+router.use('/categories', categoriesRoutes);
 
 export default router;
