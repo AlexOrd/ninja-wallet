5ebc06536de5c24fcf6364f6f14f6da37a17dc69
Vitalii <ghvitaliidrapaliuk@gmail.com>
2021-04-20 21:33:44 +0300

    added axios-configs

---
+++ client/utils/auth/constants.js
@@ -0,0 +1,8 @@
+export const SUCCEEDED = 'SUCCEEDED';
+export const FAILED = 'FAILED';
+export const LOADING = 'LOADING';
+export const IDLE = 'IDLE';
+export const ACCESS_TOKEN_STORAGE_NAME = 'accessToken';
+export const REFRESH_TOKEN_STORAGE_NAME = 'refreshToken';
+export const ACCESS_TOKEN_HEADERS_NAME = 'access-token';
+export const REFRESH_TOKEN_HEADERS_NAME = 'refresh-token';
