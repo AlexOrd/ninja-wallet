978a3d2d9cda75f564482adfe8372ad0b6bac1be
mariiapolova19 <mariia.polova@eliftech.com>
2021-04-20 19:19:32 +0300

    Speed up of bundling during module replacement

--- server/app.js
+++ server/app.js
@@ -4,11 +4,12 @@ import routes from './routes/index.route';
 import * as errorHandler from './middlewares/errorHandler';
 import joiErrorHandler from './middlewares/joiErrorHandler';
 import requestLogger from './middlewares/requestLogger';
+import chokidar from 'chokidar';
 require('dotenv').config();
 
 import { connect as connectToDB } from './config/database';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './config/swagger'
+import swaggerDocument from './config/swagger';
 
 // enable webpack hot module replacement in development mode
 import webpack from 'webpack';
@@ -17,17 +18,37 @@ import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack/webpack.config.dev';
 
 import { checkAccessAndProvideUserID } from './middlewares/auth/route_verifiers';
-import authRoutes from './routes/auth.route'
+import authRoutes from './routes/auth.route';
 import Transaction from './models/transaction.model';
 import { deviceDetector } from './middlewares/auth/device_detector';
 import { getDeviceInfo } from './utils/auth/aux_functions/get_device_info';
 
 if (process.env.NODE_ENV === 'development') {
-    const compiler = webpack(webpackConfig);
-    app.use(
-        webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath })
-    );
-    app.use(webpackHotMiddleware(compiler));
+  const compiler = webpack(webpackConfig);
+  app.use(
+    webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath })
+  );
+  app.use(webpackHotMiddleware(compiler));
+
+  const watcher = chokidar.watch('./server');
+
+  watcher.on('ready', function () {
+    watcher.on('all', function () {
+      console.log('Clearing /server/ module cache from server');
+      Object.keys(require.cache).forEach(function (id) {
+        if (/[\/\\]server[\/\\]/.test(id)) delete require.cache[id];
+      });
+    });
+  });
+
+  // Do "hot-reloading" of react stuff on the server
+  // Throw away the cached client modules and let them be re-required next time
+  compiler.plugin('done', function () {
+    console.log('Clearing /client/ module cache from server');
+    Object.keys(require.cache).forEach(function (id) {
+      if (/[\/\\]client[\/\\]/.test(id)) delete require.cache[id];
+    });
+  });
 }
 
 connectToDB();
@@ -41,8 +62,8 @@ app.use('/', routes);
 
 // Landing page
 app.get('*', (req, res) => {
-    // res.send('hello world')
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+  // res.send('hello world')
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // Joi Error Handler Middleware
@@ -54,7 +75,7 @@ app.use(errorHandler.notFound);
 app.use(errorHandler.methodNotAllowed);
 
 app.listen(app.get('port'), app.get('host'), () => {
-    console.log(`Server running at http://${app.get('host')}:${app.get('port')}`);
+  console.log(`Server running at http://${app.get('host')}:${app.get('port')}`);
 });
 
 export default app;
